9fc27499493e50b1e063d35da6b2d97a993ad2b1
ChronoSayo <chronosayo@gmail.com>
2022-01-30 22:49:31 +0100

    Fixed everything except for small issues.

--- SuperSetTimer/SuperSetTimer/MainPage.xaml.cs
+++ SuperSetTimer/SuperSetTimer/MainPage.xaml.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -13,8 +10,12 @@ namespace SuperSetTimer
 {
     public partial class MainPage : ContentPage
     {
-        private Timer _timer;
-        private Stopwatch _stopWatch;
+        private readonly Timer _timer;
+        private readonly Stopwatch _stopWatch;
+        private int _sets;
+        private bool _isCooldown, _firstSet;
+
+        private int Sets => int.Parse(SetsEntry.Text);
 
         public MainPage()
         {
@@ -23,9 +24,12 @@ namespace SuperSetTimer
 
             _timer = new Timer();
             _timer.Elapsed += OnTimerTick;
-            _timer.Interval = 100;
+            _timer.Interval = 1;
             _timer.Enabled = false;
             _timer.AutoReset = true;
+
+            _isCooldown = false;
+            _firstSet = true;
 
             _stopWatch = new Stopwatch();
             TimerLabel.Text = "0.0";
@@ -33,9 +37,43 @@ namespace SuperSetTimer
 
         private void OnTimerTick(object sender, ElapsedEventArgs e)
         {
-            int setTimer = int.Parse(SecondsEntry.Text);
+            int setTimer;
+            if (_firstSet)
+                setTimer = 2;
+            else
+                setTimer = _isCooldown ? int.Parse(CooldownEntry.Text) : int.Parse(ActiveEntry.Text);
             TimeSpan remainingTimer = TimeSpan.FromSeconds(setTimer - _stopWatch.Elapsed.Seconds);
-            MainThread.BeginInvokeOnMainThread(() => { TimerLabel.Text = remainingTimer.ToString(@"m\:ss"); });
+
+            int showSetsLeft = Sets - _sets;
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                TimerLabel.Text = "Sets: " + showSetsLeft + "/" + SetsEntry.Text + "\n";
+                TimerLabel.Text += remainingTimer.ToString(@"m\:ss");
+            });
+
+            if (remainingTimer.Seconds < 0)
+            {
+                if (_firstSet)
+                    _firstSet = false;
+
+                _isCooldown = !_isCooldown;
+                if (!_isCooldown)
+                {
+                    _sets--;
+                    StatusLabel.Text = "GO!";
+                }
+                else
+                    StatusLabel.Text = "Cooldown";
+
+                _stopWatch.Restart();
+            }
+
+            if (_sets >= 0) 
+                return;
+
+            _timer.Stop();
+            EnableEntries(true);
+            _stopWatch.Restart();
         }
 
         private void OnTimerStart(object sender, EventArgs e)
@@ -43,12 +81,38 @@ namespace SuperSetTimer
             _stopWatch.Start();
             _timer.Enabled = true;
             _timer.Start();
+            _isCooldown = true;
+            _sets = int.Parse(SetsEntry.Text);
+            _firstSet = true;
+            StatusLabel.Text = "Get ready!";
+
+            EnableEntries(false);
+
         }
         private void OnTimerStop(object sender, EventArgs e)
         {
             _stopWatch.Stop();
             _timer.Enabled = false;
             _timer.Stop();
+            StatusLabel.Text = "Paused";
+        }
+        private void OnTimerReset(object sender, EventArgs e)
+        {
+            _stopWatch.Stop();
+            _stopWatch.Reset();
+            _timer.Enabled = false;
+            _timer.Stop();
+            _firstSet = true;
+            StatusLabel.Text = "Starting over.";
+
+            EnableEntries(true);
+        }
+
+        private void EnableEntries(bool enable)
+        {
+            CooldownEntry.IsEnabled = enable;
+            ActiveEntry.IsEnabled = enable;
+            SetsEntry.IsEnabled = enable;
         }
     }
 }
