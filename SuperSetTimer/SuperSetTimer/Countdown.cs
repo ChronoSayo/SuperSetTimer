5596dd7e24737e96b3ae2168beb2b88fcb0f83e5
ChronoSayo <chronosayo@gmail.com>
2022-02-27 01:20:51 +0100

    Organized the code. Doesn't work though

---
+++ SuperSetTimer/SuperSetTimer/Countdown.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SuperSetTimer
+{
+    class Countdown
+    {
+        private readonly Timer _timer;
+        private readonly Stopwatch _stopWatch;
+        private int _sets;
+        private bool _isCooldown, _startUp, _paused;
+
+        private enum State
+        {
+            StandBy, StartUp, Active, Cooldown, Paused
+        }
+
+        public Entry StartUpEntry { get; set; }
+        public Entry ActiveEntry { get; set; }
+        public Entry CooldownEntry { get; set; }
+        public Entry SetsEntry { get; set; }
+        public Label TimerLabel { get; set; }
+        public Label StatusLabel { get; set; }
+        public Frame StatusFrame { get; set; }
+
+        private int StartUpTime => int.Parse(StartUpEntry.Text);
+        private int ActiveTime => int.Parse(ActiveEntry.Text);
+        private int CooldownTime => int.Parse(CooldownEntry.Text);
+        private int Sets => int.Parse(SetsEntry.Text);
+
+        private string TimerText
+        {
+            get => TimerLabel.Text;
+            set => TimerLabel.Text = value;
+        }
+        private string StatusText
+        {
+            set => StatusLabel.Text = value;
+        }
+
+        public Countdown()
+        {
+            _timer = new Timer();
+            _timer.Elapsed += OnTimerTick;
+            _timer.Interval = 1;
+            _timer.Enabled = false;
+            _timer.AutoReset = true;
+
+            _isCooldown = false;
+            _startUp = true;
+            _paused = false;
+
+            _stopWatch = new Stopwatch();
+            TimerText = "0.0";
+        }
+
+        private void OnTimerTick(object sender, ElapsedEventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                int setTimer;
+                if (_startUp)
+                    setTimer = StartUpTime;
+                else
+                    setTimer = _isCooldown ? CooldownTime : ActiveTime;
+                TimeSpan remainingTimer = TimeSpan.FromSeconds(setTimer) - _stopWatch.Elapsed;
+
+                TimerText = "Sets: " + _sets + "/" + Sets + "\n";
+                TimerText += remainingTimer.ToString(@"m\:ss\.ff");
+
+                if (remainingTimer.Seconds >= 0 && remainingTimer.Milliseconds >= 0)
+                    return;
+
+                _isCooldown = !_isCooldown;
+
+                if (_startUp)
+                    _startUp = false;
+
+                if (!_isCooldown)
+                {
+                    _sets++;
+                    SetVisualsByState(State.Active);
+                }
+                else
+                {
+                    if (_sets >= Sets)
+                    {
+                        SetVisualsByState(State.StandBy);
+                        EnableEntries(true);
+                        Reset();
+                        return;
+                    }
+                    SetVisualsByState(State.Cooldown);
+                }
+
+                _stopWatch.Restart();
+            });
+        }
+
+        public void Start()
+        {
+            _stopWatch.Start();
+            _timer.Enabled = true;
+            _timer.Start();
+            if (!_paused)
+            {
+                _isCooldown = true;
+                _startUp = true;
+                _sets = 0;
+                SetVisualsByState(State.Active);
+            }
+            else
+                _paused = false;
+
+            EnableEntries(false);
+
+        }
+        public void Stop()
+        {
+            _stopWatch.Stop();
+            _timer.Enabled = false;
+            _timer.Stop();
+            StatusText = "Paused";
+            _paused = true;
+            SetVisualsByState(State.StandBy);
+        }
+        public void TimerReset()
+        {
+            SetVisualsByState(State.StandBy);
+            Reset();
+        }
+
+        private void EnableEntries(bool enable)
+        {
+            CooldownEntry.IsEnabled = enable;
+            ActiveEntry.IsEnabled = enable;
+            SetsEntry.IsEnabled = enable;
+            StartUpEntry.IsEnabled = enable;
+        }
+
+        private void SetVisualsByState(State state)
+        {
+            switch (state)
+            {
+                case State.StandBy:
+                    StatusText = "DONE";
+                    StatusFrame.BackgroundColor = Color.AliceBlue;
+                    break;
+                case State.StartUp:
+                    StatusText = "Get ready!";
+                    StatusFrame.BackgroundColor = Color.CadetBlue;
+                    break;
+                case State.Active:
+                    StatusText = "GO!";
+                    StatusFrame.BackgroundColor = Color.LawnGreen;
+                    break;
+                case State.Cooldown:
+                    StatusText = "Rest";
+                    StatusFrame.BackgroundColor = Color.Yellow;
+                    break;
+                case State.Paused:
+                    StatusText = "Paused";
+                    StatusFrame.BackgroundColor = Color.Red;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+        }
+
+        private void Reset()
+        {
+            _stopWatch.Stop();
+            _stopWatch.Reset();
+
+            _timer.Stop();
+            _timer.Enabled = false;
+
+            _startUp = true;
+            TimerText = "0.0";
+            EnableEntries(true);
+        }
+    }
+}
--- SuperSetTimer/SuperSetTimer/MainPage.xaml.cs
+++ SuperSetTimer/SuperSetTimer/MainPage.xaml.cs
@@ -11,143 +11,33 @@ namespace SuperSetTimer
 {
     public partial class MainPage : ContentPage
     {
-        private readonly Timer _timer;
-        private readonly Stopwatch _stopWatch;
-        private int _sets;
-        private bool _isCooldown, _startUp, _paused;
-
-        private int StartUpTime => int.Parse(StartUpEntry.Text);
-        private int ActiveTime => int.Parse(ActiveEntry.Text);
-        private int CooldownTime => int.Parse(CooldownEntry.Text);
-        private int Sets => int.Parse(SetsEntry.Text);
-
-        private string TimerText
-        {
-            get => TimerLabel.Text;
-            set => TimerLabel.Text = value;
-        }
-        private string StatusText
-        {
-            set => StatusLabel.Text = value;
-        }
-
+        private Countdown _countdown;
         public MainPage()
         {
-            InitializeComponent();
-            BindingContext = this;
-
-            _timer = new Timer();
-            _timer.Elapsed += OnTimerTick;
-            _timer.Interval = 10;
-            _timer.Enabled = false;
-            _timer.AutoReset = true;
-
-            _isCooldown = false;
-            _startUp = true;
-            _paused = false;
-
-            _stopWatch = new Stopwatch();
-            TimerText = "0.0";
-        }
-
-        private void OnTimerTick(object sender, ElapsedEventArgs e)
-        {
-            int setTimer;
-            if (_startUp)
-                setTimer = StartUpTime;
-            else
-                setTimer = _isCooldown ? CooldownTime : ActiveTime;
-            TimeSpan remainingTimer = TimeSpan.FromSeconds(setTimer) - _stopWatch.Elapsed;
-            
-            MainThread.BeginInvokeOnMainThread(() =>
+            _countdown = new Countdown
             {
-                TimerText = "Sets: " + _sets + "/" + Sets + "\n";
-                TimerText += remainingTimer.ToString(@"m\:ss\.ff");
-            });
-
-            if (remainingTimer.Seconds >= 0 && remainingTimer.Milliseconds >= 0)
-                return;
-
-            _isCooldown = !_isCooldown;
-
-            if (_startUp)
-            {
-                _startUp = false;
-                _stopWatch.Restart();
-                return;
-            }
-
-            if (!_isCooldown)
-            {
-                _sets++; 
-                StatusText = "GO!";
-            }
-            else
-            {
-                if (_sets > Sets)
-                {
-                    StatusText = "DONE!";
-                    EnableEntries(true);
-                    Reset();
-                    return;
-                }
-                StatusText = "Rest";
-            }
-
-            _stopWatch.Restart();
+                StartUpEntry = StartUpEntry,
+                ActiveEntry = ActiveEntry,
+                CooldownEntry = CooldownEntry,
+                SetsEntry = SetsEntry,
+                StatusLabel = StatusLabel,
+                TimerLabel = TimerLabel
+            };
         }
 
         private void OnTimerStart(object sender, EventArgs e)
         {
-            _stopWatch.Start();
-            _timer.Enabled = true;
-            _timer.Start();
-            if (!_paused)
-            {
-                _isCooldown = true;
-                _startUp = true;
-                _sets = 0;
-                StatusText = "Get ready!";
-            }
-            else
-                _paused = false;
-
-            EnableEntries(false);
-
+            _countdown.Start();
         }
+
         private void OnTimerStop(object sender, EventArgs e)
         {
-            _stopWatch.Stop();
-            _timer.Enabled = false;
-            _timer.Stop();
-            StatusText = "Paused";
-            _paused = true;
+            _countdown.Stop();
         }
+
         private void OnTimerReset(object sender, EventArgs e)
         {
-            StatusText = "Starting over.";
-            Reset();
-        }
-
-        private void EnableEntries(bool enable)
-        {
-            CooldownEntry.IsEnabled = enable;
-            ActiveEntry.IsEnabled = enable;
-            SetsEntry.IsEnabled = enable;
-            StartUpEntry.IsEnabled = enable;
-        }
-
-        private void Reset()
-        {
-            _stopWatch.Stop();
-            _stopWatch.Reset();
-
-            _timer.Enabled = false;
-            _timer.Stop();
-
-            _startUp = true;
-            TimerText = "0.0";
-            EnableEntries(true);
+            _countdown.TimerReset();
         }
     }
 }
