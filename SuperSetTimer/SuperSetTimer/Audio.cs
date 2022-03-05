c9654be40b46f300dcd35e5ca7740a3b25169877
ChronoSayo <chronosayo@gmail.com>
2022-03-05 16:37:35 +0100

    Added voice clips.

--- SuperSetTimer/SuperSetTimer/Audio.cs
+++ SuperSetTimer/SuperSetTimer/Audio.cs
@@ -11,50 +11,46 @@ namespace SuperSetTimer
         public RadioButton EffectRadioButton { get; set; }
 
         private bool Mute => MuteRadioButton.IsChecked;
+        private bool Effects => EffectRadioButton.IsChecked;
 
         public Audio()
         {
             _player = CrossSimpleAudioPlayer.Current;
         }
 
-        public void SetSoundOptions(bool effects)
-        {
-
-        }
-
         public void PlayDone()
         {
             if(Mute)
                 return;
-            _player.Load("done.wav");
+            _player.Load(Effects ? "done.wav" : "game.wav");
             _player.Play();
         }
         public void PlayStartUp()
         {
             if (Mute)
                 return;
-            _player.Load("startup.wav");
+            _player.Load(Effects ? "startup.wav" : "ready.wav");
             _player.Play();
         }
         public void PlayActive()
         {
             if (Mute)
                 return;
-            _player.Load("active.ogg");
+            _player.Load(Effects ? "active.ogg" : "go.wav");
             _player.Play();
         }
         public void PlayCooldown()
         {
             if (Mute)
                 return;
-            _player.Load("cooldown.ogg");
+            _player.Load(Effects ? "cooldown.ogg" : "time.wav");
             _player.Play();
         }
         public void PlayPause()
         {
             if (Mute)
                 return;
-            _player.Load("pause.ogg");
+            _player.Load(Effects ? "pause.ogg" : "continue.wav");
             _player.Play();
         }
     }
